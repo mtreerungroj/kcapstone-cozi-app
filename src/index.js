ee7e8fa4a8c02e9e1f02291619748ae99f62268f
mtreerungroj <m.treerungroj@gmail.com>
2018-07-15 18:50:04 +0700

    setup Firebase

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import firebase from 'firebase/app'
+
+var config = {
+  apiKey: 'AIzaSyCTmDtrjWAZXJXHlANWjT4MEt9vl9Dnef4',
+  authDomain: 'coziplus-kcapstone.firebaseapp.com',
+  databaseURL: 'https://coziplus-kcapstone.firebaseio.com',
+  projectId: 'coziplus-kcapstone',
+  storageBucket: '',
+  messagingSenderId: '1066197496793'
+}
+firebase.initializeApp(config)
 
 ReactDOM.render(<App />, document.getElementById('root'))
