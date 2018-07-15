44d365394ffb12c189d13e95f53f8ce778c3ec6b
mtreerungroj <m.treerungroj@gmail.com>
2018-07-15 15:56:09 +0700

    login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import Button from '@material-ui/core/Button'
+import Login from './Login'
+
+// import { createMuiTheme } from '@material-ui/core/styles'
+
+// // Theme
+// const theme = createMuiTheme({
+//   fontFamily: 'Kanit, sans-serif',
+//   palette: {
+//     primary: {
+//       main: '#FFFACD'
+//     },
+//     secondary: {
+//       main: '#000000'
+//     }
+//   }
+// })
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isAuth: false
+    }
+  }
+
+  toggleAuth = () => {
+    this.setState({ isAuth: !this.state.isAuth })
+    console.log(this.state)
+  }
+
   render () {
     return (
-      <Button variant='contained' color='primary'>
-        Hello World
-      </Button>
+      <div
+        style={{
+          height: '100vh'
+          // backgroundColor: 'red'
+        }}
+      >
+        <Login toggleAuth={this.toggleAuth} />
+      </div>
     )
   }
 }
---
+++ src/Login.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+
+import logo from './assets/images/logo.png'
+import loginwith from './assets/images/login-with.png'
+
+class Login extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      rememberme: false
+    }
+  }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked })
+  }
+
+  render () {
+    return (
+      <div>
+        <img src={logo} style={{ width: '100%' }} />
+        <div
+          style={{
+            marginLeft: 80,
+            marginRight: 80,
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          <TextField
+            id='username'
+            label='Username'
+            placeholder='Username'
+            margin='normal'
+            underlineStyle={{ borderColor: 'white' }}
+          />
+          <TextField
+            id='password'
+            label='Password'
+            placeholder='Password'
+            margin='normal'
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={this.state.checked}
+                onChange={this.handleChange('rememberme')}
+                value='rememberme'
+              />
+            }
+            label='Remember me'
+          />
+          <div
+            style={{ marginTop: 20, width: 'fitContent', textAlign: 'center' }}
+          >
+            <Button
+              onClick={this.props.toggleAuth}
+              variant='outlined'
+              color='#FFFACD'
+            >
+              Login
+            </Button>
+          </div>
+          <div style={{ textAlign: 'center', marginTop: 20 }}>
+            <Typography variant='body1'>
+              <a href='url'>Forgot password?</a>
+            </Typography>
+          </div>
+          <div style={{ textAlign: 'center', marginTop: 20, marginBottom: 10 }}>
+            <Typography variant='body1'>
+              or log in with
+            </Typography>
+          </div>
+          <img src={loginwith} style={{ width: '100%' }} />
+          <div style={{ textAlign: 'center', marginTop: 20 }}>
+            <Typography variant='body1'>
+              Create new account <a href='url'>Sign up</a>
+            </Typography>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login
