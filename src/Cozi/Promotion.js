5dc4ed7909d0edc32546c1c994e7457d9e2fe826
mtreerungroj <m.treerungroj@gmail.com>
2018-07-17 11:53:27 +0700

    add promotion page

--- src/Cozi/Promotion.js
+++ src/Cozi/Promotion.js
@@ -4,10 +4,42 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+
+import 'simplebar' // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+import 'simplebar/dist/simplebar.css'
+
+import promotion1 from '../assets/images/promotion1.PNG'
+import promotion2 from '../assets/images/promotion2.PNG'
+import promotion3 from '../assets/images/promotion3.PNG'
+import promotion4 from '../assets/images/promotion4.PNG'
+import promotion5 from '../assets/images/promotion5.PNG'
+import promotion6 from '../assets/images/promotion6.PNG'
+import promotion7 from '../assets/images/promotion7.PNG'
+import promotion8 from '../assets/images/promotion8.PNG'
+import promotion9 from '../assets/images/promotion9.PNG'
+
 export default class Promotion extends Component {
   constructor (props) {
     super(props)
     this.state = {}
+  }
+
+  renderPromotion = promotion => {
+    return (
+      <Card>
+        <img
+          src={promotion}
+          alt={promotion}
+          style={{
+            width: '100%',
+            height: 'auto'
+          }}
+        />
+      </Card>
+    )
   }
 
   render () {
@@ -31,7 +63,17 @@ export default class Promotion extends Component {
           </AppBar>
         </div>
         <div style={{ backgroundColor: 'pink', display: 'flex', flex: 1 }}>
-          Promotion
+          <div data-simplebar style={{ height: 623 }}>
+            {this.renderPromotion(promotion1)}
+            {this.renderPromotion(promotion2)}
+            {this.renderPromotion(promotion3)}
+            {this.renderPromotion(promotion4)}
+            {this.renderPromotion(promotion5)}
+            {this.renderPromotion(promotion6)}
+            {this.renderPromotion(promotion7)}
+            {this.renderPromotion(promotion8)}
+            {this.renderPromotion(promotion9)}
+          </div>
         </div>
       </div>
     )
