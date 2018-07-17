74e094aa385e4102f88cc840b3ef03335662b07c
mtreerungroj <m.treerungroj@gmail.com>
2018-07-17 16:50:48 +0700

    add point history

--- src/Cozi/PointHistory.js
+++ src/Cozi/PointHistory.js
@@ -3,17 +3,44 @@ import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Divider from '@material-ui/core/Divider'
 
 import IconButton from '@material-ui/core/IconButton'
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft'
 
+import 'simplebar' // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+import 'simplebar/dist/simplebar.css'
+
+import his1 from '../assets/images/his1.PNG'
+import his2 from '../assets/images/his2.PNG'
+import his3 from '../assets/images/his3.PNG'
+import his4 from '../assets/images/his4.PNG'
+import his5 from '../assets/images/his5.PNG'
+import his6 from '../assets/images/his6.PNG'
+import his7 from '../assets/images/his7.PNG'
+import his8 from '../assets/images/his8.PNG'
+
 export default class PointHistory extends Component {
+  renderHistory = history => (
+    <div>
+      <ListItem button style={{ padding: 0 }}>
+        <img
+          src={history}
+          alt={history}
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </ListItem>
+      <Divider />
+    </div>
+  )
+
   render () {
     return (
       <div
         style={{
-          backgroundColor: 'red',
-          height: '100vh',
+          height: '100%',
           display: 'flex',
           flexDirection: 'column',
           flex: 1
@@ -35,8 +62,18 @@ export default class PointHistory extends Component {
             </Toolbar>
           </AppBar>
         </div>
-        <div style={{ backgroundColor: 'lightblue', display: 'flex', flex: 1 }}>
-          Point History
+        <div
+          data-simplebar
+          style={{ display: 'flex', flex: 1, height: '100%' }}
+        >
+          {this.renderHistory(his1)}
+          {this.renderHistory(his2)}
+          {this.renderHistory(his3)}
+          {this.renderHistory(his4)}
+          {this.renderHistory(his5)}
+          {this.renderHistory(his6)}
+          {this.renderHistory(his7)}
+          {this.renderHistory(his8)}
         </div>
       </div>
     )
