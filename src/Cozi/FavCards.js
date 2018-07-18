72e6cd666441e450bc809bbba74c49c3c88e74dd
mtreerungroj <m.treerungroj@gmail.com>
2018-07-18 10:41:24 +0700

    add more fav cards

--- src/Cozi/FavCards.js
+++ src/Cozi/FavCards.js
@@ -1,50 +1,87 @@
 import React, { Component } from 'react'
+
 import cardStarbuck from '../assets/images/card-starbuck.png'
+import cardCosta from '../assets/images/card-costa.png'
+import cardCoffeebean from '../assets/images/card-coffeebean.jpg'
+import cardLoft from '../assets/images/card-loft.png'
+import cardMk from '../assets/images/card-mk.jpg'
+import cardWatsons from '../assets/images/card-watsons.png'
+import cardEtude from '../assets/images/card-etude.png'
+import cardPtt from '../assets/images/card-ptt.png'
+import cardAirways from '../assets/images/card-airways.jpg'
+import cardOnecard from '../assets/images/card-onecard.png'
 
 import 'simplebar' // or "import SimpleBar from 'simplebar';" if you want to use it manually.
 import 'simplebar/dist/simplebar.css'
 
 export default class Profile extends Component {
-  renderCard = () => (
-    <img
-      src={cardStarbuck}
-      alt='card-starbuck'
-      style={{ width: '100%', height: 'auto' }}
-    />
-  )
-
-  renderRowCard = () => (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <div
-        style={{
-          flex: 1,
-          margin: 10,
-          marginRight: 5
-        }}
-      >
-        {this.renderCard()}
-      </div>
-      <div
-        style={{
-          flex: 1,
-          margin: 10,
-          marginLeft: 5
-        }}
-      >
-        {this.renderCard()}
-      </div>
-    </div>
+  renderCard = card => (
+    <img src={card} alt={card} style={{ width: '100%', height: 'auto' }} />
   )
 
   render () {
     return (
       <div data-simplebar style={{ height: 343 }}>
-        {this.renderRowCard()}
-        {this.renderRowCard()}
-        {this.renderRowCard()}
-        {this.renderRowCard()}
-        {this.renderRowCard()}
+
+        <div style={styles.outerContainer}>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardStarbuck)}
+          </div>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardCosta)}
+          </div>
+        </div>
+
+        <div style={styles.outerContainer}>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardCoffeebean)}
+          </div>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardLoft)}
+          </div>
+        </div>
+
+        <div style={styles.outerContainer}>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardWatsons)}
+          </div>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardMk)}
+          </div>
+        </div>
+
+        <div style={styles.outerContainer}>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardEtude)}
+          </div>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardPtt)}
+          </div>
+        </div>
+
+        <div style={styles.outerContainer}>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardAirways)}
+          </div>
+          <div style={styles.innerContainer}>
+            {this.renderCard(cardOnecard)}
+          </div>
+        </div>
+
       </div>
     )
+  }
+}
+
+const styles = {
+  outerContainer: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  innerContainer: {
+    flex: 1,
+    margin: 5,
+    marginLeft: 10,
+    marginRight: 10
   }
 }
