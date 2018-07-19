84c93b4c1a49d57cc5bb3ef3e80f70fa24c33a3e
mtreerungroj <m.treerungroj@gmail.com>
2018-07-19 15:39:27 +0700

    add noti and search button

--- src/Cozi/MyCards.js
+++ src/Cozi/MyCards.js
@@ -4,6 +4,10 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
 import Avatar from '@material-ui/core/Avatar'
+import Badge from '@material-ui/core/Badge'
+import IconButton from '@material-ui/core/IconButton'
+import Notifications from '@material-ui/icons/Notifications'
+import Search from '@material-ui/icons/Search'
 
 import 'simplebar' // or "import SimpleBar from 'simplebar';" if you want to use it manually.
 import 'simplebar/dist/simplebar.css'
@@ -46,9 +50,46 @@ export default class MyCards extends Component {
         <div>
           <AppBar position='static' color='secondary'>
             <Toolbar>
-              <Typography variant='title' color='primary' align='center'>
+              <Typography
+                variant='title'
+                color='primary'
+                align='center'
+                style={{ flexGrow: 1, marginLeft: 100 }}
+              >
                 My Cards
               </Typography>
+
+              {this.props.badge
+                ? <IconButton
+                  color='inherit'
+                  aria-label='notifications'
+                  style={{ marginRight: 0 }}
+                  >
+                  <Badge
+                    badgeContent={'3'}
+                    color='error'
+                    onClick={() => this.props.openNotifications('mycards')}
+                    >
+                    <Notifications />
+                  </Badge>
+                </IconButton>
+                : <IconButton
+                  color='inherit'
+                  aria-label='notifications'
+                  onClick={() => this.props.openNotifications('mycards')}
+                  style={{ marginRight: 0 }}
+                  >
+                  <Notifications />
+                </IconButton>}
+
+              <IconButton
+                color='inherit'
+                aria-label='search'
+                onClick={() => this.props.openSearch('mycards')}
+              >
+                <Search />
+              </IconButton>
+
             </Toolbar>
           </AppBar>
         </div>
